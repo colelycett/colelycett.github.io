1231251171707eb3676b5283d96d5fcb05c3b9f9
colelycett <colelycett@umail.ucsb.edu>
2025-08-28 19:02:51 -0700

    Get functionality added

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get((req, res) => {
+    res.status(200).send("Hey, you made a get request!");
+})
+
 app.listen(() => {
     console.log("Alive?");
 })
